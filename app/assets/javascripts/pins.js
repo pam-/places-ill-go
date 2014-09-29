9abbbf94426311678ffd3ead005e9ed841214194
pam- <pam.assogba@gmail.com>
2014-09-28 20:49:07 -0400

    working on css

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -1,3 +1,5 @@
+var geocoder;
+
 function initialize() {
 	var mapOptions = {
 		zoom: 3,
@@ -6,11 +8,14 @@ function initialize() {
 
 	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+	geocoder = new google.maps.Geocoder();
+
 	// display latitudes
 	$.ajax({
 		url: '/pins.json',
 		dataType: 'json',
 		success: function(result){
+			codeLatLng(result);
 			return render(result);
 		}
 	})
@@ -25,10 +30,33 @@ function initialize() {
 		};
 	}
 
+	function codeLatLng(result){
+		var ul = document.getElementsByTagName('ul')[0];
+
+		for(i=0; i < result.length; i++){
+			pin = result[i];
+			var lat = pin.latitude;
+			var lng = pin.longitude;
+			var latLng = new google.maps.LatLng(lat, lng);
+			geocoder.geocode({'latLng': latLng}, function(results, status){
+				if (status == google.maps.GeocoderStatus.OK) {
+					if (results[1]) {
+						var li = document.createElement('li');
+						li.appendChild(document.createTextNode(results[1].formatted_address));
+						ul.appendChild(li);					
+					} else {
+						alert('No results found');
+					}
+				}	else {
+					alert('Geocoder failed due to: ' + status);
+				}
+			});
+		}
+	}
+
 	google.maps.event.addListener(map, 'click', function(event) {
 		var latitude = event.latLng.lat();
 		var longitude = event.latLng.lng();
-		// alert(latitude)
 		newLocation = new google.maps.LatLng(latitude, longitude);
 	  $.ajax({
 	  	type: 'POST',
@@ -43,8 +71,14 @@ function initialize() {
 	  		position: newLocation,
 	  		map: map
 	  	})
-  	});
+  	})
 	});
+
+	var expander = document.getElementById('expand');
+	expander.addEventListener('click', function(){
+		var mapContainer = document.getElementsByClassName('map-container')[0];
+		mapContainer.classList.toggle('expanded');
+	})
 }
 
 function loadScript() {
