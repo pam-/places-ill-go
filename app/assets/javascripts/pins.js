354708089f537d626e6c8a45db21c7d4cd4cc5d8
pam- <pam.assogba@gmail.com>
2014-09-29 16:46:06 -0400

    working on displaying locations

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -15,14 +15,17 @@ function initialize() {
 		url: '/pins.json',
 		dataType: 'json',
 		success: function(result){
-			codeLatLng(result);
 			return render(result);
 		}
 	})
 
 	function render(result){
+		var ul = document.getElementsByTagName('ul')[0];
 		for (var i = 0; i < result.length; i++) {
 			pin = result[i];
+			var li = document.createElement('li');
+			li.appendChild(document.createTextNode(pin.latitude + ' ' + pin.longitude));
+			ul.appendChild(li);		
 			new google.maps.Marker({
 				position: new google.maps.LatLng(pin.latitude, pin.longitude),
 				map: map
@@ -30,29 +33,25 @@ function initialize() {
 		};
 	}
 
-	function codeLatLng(result){
-		var ul = document.getElementsByTagName('ul')[0];
-
-		for(i=0; i < result.length; i++){
-			pin = result[i];
-			var lat = pin.latitude;
-			var lng = pin.longitude;
-			var latLng = new google.maps.LatLng(lat, lng);
-			geocoder.geocode({'latLng': latLng}, function(results, status){
-				if (status == google.maps.GeocoderStatus.OK) {
-					if (results[1]) {
-						var li = document.createElement('li');
-						li.appendChild(document.createTextNode(results[1].formatted_address));
-						ul.appendChild(li);					
-					} else {
-						alert('No results found');
-					}
-				}	else {
-					alert('Geocoder failed due to: ' + status);
-				}
-			});
-		}
-	}
+	// function codeLatLng(){
+	// 	for(i=0; i < result.length; i++){
+	// 		pin = result[i];
+	// 		var lat = pin.latitude;
+	// 		var lng = pin.longitude;
+	// 		var latLng = new google.maps.LatLng(lat, lng);
+	// 		geocoder.geocode({'latLng': latLng}, function(results, status){
+	// 			if (status == google.maps.GeocoderStatus.OK) {
+	// 				if (results[1]) {
+									
+	// 				} else {
+	// 					alert('No results found');
+	// 				}
+	// 			}	else {
+	// 				alert('Geocoder failed due to: ' + status);
+	// 			}
+	// 		});
+	// 	}
+	// }
 
 	google.maps.event.addListener(map, 'click', function(event) {
 		var latitude = event.latLng.lat();
@@ -79,6 +78,15 @@ function initialize() {
 		var mapContainer = document.getElementsByClassName('map-container')[0];
 		mapContainer.classList.toggle('expanded');
 	})
+
+	var list = document.getElementsByTagName('li');
+	console.log(list)
+	// for(var i=0; i < list.length; i++){
+	// 	list[i].addEventListener('click', function(){
+	// 		alert('here')
+	// 	})		
+	// }
+
 }
 
 function loadScript() {
